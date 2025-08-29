9cf63f16634292415744641e2e4c817c5aa463f5
devB <devB@example.com>
2025-08-29 15:06:45 +0900

    ee

--- devB.bat
+++ devB.bat
@@ -1,5 +1,10 @@
 @echo off
 cd /d %~dp0
 
-git config user.name "soon"
+
+git config user.name "devB"
 git config user.email "devB@example.com"
+
+echo Now configured as:
+git config user.name
+git config user.email
