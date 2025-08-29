e72a316ac23b6c2a1037abdc4852148b5e9eaf37
so <tnsnz@github.com>
2025-08-29 15:12:57 +0900

    qwe

--- devB.bat
+++ devB.bat
@@ -1,5 +1,10 @@
 @echo off
 cd /d %~dp0
 
+echo Switching Git user to devB...
 git config user.name "so"
 git config user.email "tnsnz@github.com"
+
+echo Now configured as:
+git config user.name
+git config user.email
