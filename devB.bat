0338fb597d29340676b132b07f558737d36211f3
so <devB@example.com>
2025-08-29 15:07:15 +0900

    ed

--- devB.bat
+++ devB.bat
@@ -1,10 +1,5 @@
 @echo off
 cd /d %~dp0
 
-
-git config user.name "devB"
+git config user.name "so"
 git config user.email "devB@example.com"
-
-echo Now configured as:
-git config user.name
-git config user.email
